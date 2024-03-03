8601b96a093a6d44b95b9bc7ba43784177c06602
Vinicius L. Romão <vini.romao159@gmail.com>
2024-03-02 21:00:22 -0300

    Correção ex042

--- Curso em Video/Exercicios/ex042.py
+++ Curso em Video/Exercicios/ex042.py
@@ -5,14 +5,14 @@ c_side = float(input("Valor da RETA C: "))
 if a_side < b_side + c_side and b_side < a_side + c_side and c_side < b_side + a_side:
     print("É possivel formar um \033[0;33mTRIANGULO!\033[m!")
     
-    if a_side == b_side and a_side == c_side:
+    if a_side == b_side == c_side:
         print("Triangulo EQUILÁTERO!")
-        
-    elif a_side == b_side and a_side != c_side or a_side == c_side and a_side != b_side or b_side == c_side and b_side != a_side:
-        print("Triangulo ISÓSCELES!")
-        
-    if a_side != b_side and a_side != c_side and b_side !=c_side :
+    
+    elif a_side != b_side != c_side != a_side :
         print("Triangulo ESCALENO!")
+        
+    else:
+        print("Triangulo ISÓSCELES!")
         
 else:
     print("Não é possivel formar um \033[0;33mTRIANGULO\033[m!")
