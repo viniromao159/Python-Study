63acdf83242e193531a79d2cb1b9627c2ae22367
Vinicius L. Romão <vini.romao159@gmail.com>
2024-03-06 10:23:21 -0300

    Ex 62 refatorado

--- Curso em Video/Exercicios/ex062.py
+++ Curso em Video/Exercicios/ex062.py
@@ -3,26 +3,27 @@
 termo = int(input("Digite o primeiro termo: "))
 razao = int(input("Digite a razao: "))
 cont = 0
+limite = 10
 
-while cont <= 10:
+while cont <= limite:
     print(termo)
     termo += razao
     cont += 1
     
-    while cont == 10:   #Na ultima interação
+    if cont == limite:  #Na ultima interação
         print("-"*30)
         addtermo = int(input("Deseja mostrar mais termos? Digite a quantidade: "))
+        print("-"*30)
         
-        if addtermo != 0:   # Se for diferente de 0
-            cont2 = 1  # Cria outra flag
-            print("-"*30)
+        if addtermo != 0:
+            limite += addtermo  # Add a quantidade a mais de termo no limite do loop
+            print(termo)
+            termo += razao
+            cont += 1
             
-            while cont2 <= addtermo:
-                print(termo)
-                termo += razao
-                cont2 += 1
-                
         else:
             print("Encerrado!")
             cont += 1
+            
+
             
