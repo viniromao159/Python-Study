7325cf36ba78b4af9d5d6d1c58102a2f9c60bb14
Vinicius L. Romão <vini.romao159@gmail.com>
2024-03-26 11:57:45 -0300

    Corrigido os ex 78 aos 83

--- Curso em Video/Exercicios/ex078.py
+++ Curso em Video/Exercicios/ex078.py
@@ -1,8 +1,10 @@
+'''Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. 
+No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. '''
 
 valores = []
 for i in range(4):
     valor = int(input(f"Digite o {i+1}º valor: "))
     valores.append(valor)
 
-print(f"Maior valor é {max(valores)} e sua posição é {valores.index(max(valores))+1}º valor!")
-print(f"Menor valor é {min(valores)} e sua posição é {valores.index(min(valores))+1}º valor!")
+print(f"Maior valor é {max(valores)} e sua posição é {valores.index(max(valores))+1}º!")
+print(f"Menor valor é {min(valores)} e sua posição é {valores.index(min(valores))+1}º!")
--- Curso em Video/Exercicios/ex083.py
+++ Curso em Video/Exercicios/ex083.py
@@ -1,9 +1,13 @@
-expressao = input("Digite a expressão: ")
-'''exp = []
-for letra in expressao:
-    exp.append(letra)'''
+'''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
 
-if expressao.count("(") != expressao.count(")"):
+expressao = input("Digite a expressão: ")
+
+pilha = []
+for letra in expressao:
+    pilha.append(letra)
+
+if pilha.count("(") != pilha.count(")"):
     print("Expressão invalida!")
 else:
     print("Expressão valida!")
